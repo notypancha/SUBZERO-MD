1b189a03b47d53b0951de778da23cad1f6c163f3
notypancha <notypancha03@gmail.com>
2025-09-18 16:07:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;3Z1kwIAK#BI8ERDZI6zb8gPufaTTBlacUCcXl7gfG2le0lZGggd8", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;jNkCiaQB#kb4S9RwUYFZo-s1scMoRK9Fm3LG4e3ShgB-EOXC1xvk", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
